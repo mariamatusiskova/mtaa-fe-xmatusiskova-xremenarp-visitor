df520425623853a8bd4e4d8b0d44dd1f7f696aab
mariamatusiskova <xmatusiskova@stuba.sk>
2024-04-20 01:27:40 +0200

    login page done

--- composeApp/src/commonMain/kotlin/login/Login.kt
+++ composeApp/src/commonMain/kotlin/login/Login.kt
@@ -1,5 +1,6 @@
 package login
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,22 +11,35 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -47,7 +61,11 @@ fun loadLoginScreen() {
     ) {
 
         Image(
-            modifier = Modifier.fillMaxSize(1f),
+            modifier = Modifier
+                .size(width = 150.dp, height = 150.dp)
+                .padding(top = 35.dp)
+                .fillMaxSize(1f)
+                .align(Alignment.TopCenter),
             painter = painterResource(Res.drawable.logo),
             contentDescription = "Logo"
         )
@@ -55,7 +73,6 @@ fun loadLoginScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-//                    .padding(horizontal = 16.dp)
                 .padding(top = 200.dp)
                 .background(
                     color = Color(0xFFBCBE9A),
@@ -64,42 +81,113 @@ fun loadLoginScreen() {
             verticalArrangement = Arrangement.Center
         ) {
 
-            OutlinedTextField(
+            TextField(
                 value = username,
                 onValueChange = { username = it },
-                label = { Text("Username") },
+                label = {
+                    Text(
+                        "Username",
+                        style = TextStyle(
+                            color = Color.Gray,
+                            fontWeight = FontWeight.Normal
+                        )
+                    )
+                },
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Next
                 ),
-                modifier = Modifier.fillMaxWidth()
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.DarkGray,
+                    backgroundColor = Color(0xFFBCBE9A),
+                    cursorColor = Color.Gray,
+                    focusedIndicatorColor = Color.Gray,
+                    unfocusedIndicatorColor = Color.Gray
+                ),
+                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 60.dp)
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            OutlinedTextField(
+            TextField(
                 value = password,
                 onValueChange = { password = it },
-                label = { Text("Password") },
+                label = {
+                    Text(
+                        "Password",
+                        style = TextStyle(
+                            color = Color.Gray,
+                            fontWeight = FontWeight.Normal,
+                        )
+                    )
+                },
                 visualTransformation = PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Password,
                     imeAction = ImeAction.Done
                 ),
-                modifier = Modifier.fillMaxWidth()
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.DarkGray,
+                    backgroundColor = Color(0xFFBCBE9A),
+                    cursorColor = Color.Gray,
+                    focusedIndicatorColor = Color.Gray,
+                    unfocusedIndicatorColor = Color.Gray
+                ),
+                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 60.dp)
             )
 
-            Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(40.dp))
 
             Button(
                 onClick = { /* Handle login button click */ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFA1A556)),
+                shape = RoundedCornerShape(20.dp),
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = Color.Red,
-                    )
+                    .size(width = 250.dp,height = 35.dp)
+                    .align(Alignment.CenterHorizontally)
             ) {
-                Text(text = "Log in")
+                Text(
+                    text = "Log in",
+                    color = Color(0xFFE4E4E4),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            TextButton(
+                onClick = { /* Handle login button click */ },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "Forgotten password?",
+                    color = Color(0xFFE4E4E4),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            OutlinedButton(
+                onClick = { /* Handle login button click */ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                border = BorderStroke(2.dp, Color(0xFFA1A556)),
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "Sign up",
+                    color = Color(0xFFA1A556),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
     }
