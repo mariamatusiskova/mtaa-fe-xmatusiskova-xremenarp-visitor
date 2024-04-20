2fd07a03a8bee389c2056ed4bac730b69e46b200
PeterR29 <peto.remenar123@gmail.com>
2024-04-20 12:49:27 +0200

    neznam co

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -2,6 +2,7 @@ import androidx.compose.runtime.Composable
 import authentification.loadLoginScreen
 import authentification.loadResetPassword
 import authentification.loadSignupScreen
+import menu.menu
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -12,7 +13,8 @@ fun App() {
 
     //loadLoginScreen()
     //loadSignupScreen()
-    loadResetPassword()
+    //loadResetPassword()
+    menu()
 }
 
 
--- composeApp/src/commonMain/kotlin/menu/menu.kt
+++ composeApp/src/commonMain/kotlin/menu/menu.kt
@@ -25,6 +25,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
@@ -36,6 +38,15 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -62,220 +73,75 @@ import visitor.composeapp.generated.resources.logo
 @Composable
 @Preview
 fun menu() {
-
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var retype_password by remember { mutableStateOf("") }
-
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFA1A556))
+            .background(Color.White)
     ) {
-
-        Text(
-            "Reset\npassword",
-            style = MaterialTheme.typography.h4,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Start,
-            fontSize = 40.sp,
-            color = Color(0xFFE4E4E4),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 60.dp, start = 30.dp)
-        )
-
-        TextButton(
-            onClick = { /* Handle login button click */ },
-            modifier = Modifier
-                .padding(top = 20.dp, end = 16.dp)
-                .align(Alignment.TopEnd)
-        ) {
-            Text(
-                text = "X",
-                color = Color(0xFFE4E4E4),
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 200.dp)
-                .background(
-                    color = Color(0xFFBCBE9A),
-                    shape = RoundedCornerShape(topStart = 60.dp, topEnd = 60.dp)
-                ),
-            verticalArrangement = Arrangement.Center
         ) {
-
-            TextField(
-                value = email,
-                onValueChange = { email = it },
-                label = {
-                    Text(
-                        "Email",
-                        style = TextStyle(
-                            color = Color.Gray,
-                            fontWeight = FontWeight.Normal
+            // Horizontal scrolling space for images
+            LazyColumn {
+                items(50) {
+                    Button(
+                        onClick = { /* Handle login button click */ },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFA1A556)),
+                        shape = RoundedCornerShape(20.dp),
+                        modifier = Modifier
+                            .size(width = 250.dp,height = 35.dp)
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(
+                            text = "Log in",
+                            color = Color(0xFFE4E4E4),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
                         )
-                    )
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    imeAction = ImeAction.Next
-                ),
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.DarkGray,
-                    backgroundColor = Color(0xFFBCBE9A),
-                    cursorColor = Color.Gray,
-                    focusedIndicatorColor = Color.Gray,
-                    unfocusedIndicatorColor = Color.Gray
-                ),
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 60.dp)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            TextField(
-                value = password,
-                onValueChange = { password = it },
-                label = {
-                    Text(
-                        "Password",
-                        style = TextStyle(
-                            color = Color.Gray,
-                            fontWeight = FontWeight.Normal,
-                        )
-                    )
-                },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                ),
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.DarkGray,
-                    backgroundColor = Color(0xFFBCBE9A),
-                    cursorColor = Color.Gray,
-                    focusedIndicatorColor = Color.Gray,
-                    unfocusedIndicatorColor = Color.Gray
-                ),
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 60.dp)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            TextField(
-                value = retype_password,
-                onValueChange = { retype_password = it },
-                label = {
-                    Text(
-                        "Retype password",
-                        style = TextStyle(
-                            color = Color.Gray,
-                            fontWeight = FontWeight.Normal,
-                        )
-                    )
-                },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done
-                ),
-                colors = TextFieldDefaults.textFieldColors(
-                    textColor = Color.DarkGray,
-                    backgroundColor = Color(0xFFBCBE9A),
-                    cursorColor = Color.Gray,
-                    focusedIndicatorColor = Color.Gray,
-                    unfocusedIndicatorColor = Color.Gray
-                ),
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 60.dp)
-            )
-
-            Spacer(modifier = Modifier.height(40.dp))
-
-            Button(
-                onClick = { /* Handle login button click */ },
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFA1A556)),
-                shape = RoundedCornerShape(20.dp),
-                modifier = Modifier
-                    .size(width = 250.dp,height = 35.dp)
-                    .align(Alignment.CenterHorizontally)
-            ) {
-                Text(
-                    text = "Continue",
-                    color = Color(0xFFE4E4E4),
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold
-                )
+                    }
+                }
             }
-            BottomNavigation {
-                BottomNavigationItem(
-                    icon = {  },
-                    label = { Text("Home3") },
-                    selected = false,
-                    onClick = { /* Handle home navigation */ }
-                )
-                BottomNavigationItem(
-                    icon = { },
-                    label = { Text("Home 4") },
-                    selected = false,
-                    onClick = { /* Handle search navigation */ }
-                )
-                BottomNavigationItem(
-                    icon = {  },
-                    label = { Text("Home 5") },
-                    selected = false,
-                    onClick = { /* Handle notifications navigation */ }
-                )
-                BottomNavigationItem(
-                    icon = {  },
-                    label = { Text("Home 5") },
-                    selected = false,
-                    onClick = { /* Handle notifications navigation */ }
-                )
-            }
+
+
         }
-
+        // Bottom Navigation
+        BottomNavigation(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 800.dp), // Adjust as needed
+            backgroundColor = Color.White
+        ) {
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Person, contentDescription = "Person") },
+                selected = false,
+                onClick = { /* Handle home navigation */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.LocationOn, contentDescription = "Location") },
+                selected = false,
+                onClick = { /* Handle search navigation */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Add, contentDescription = "Add") },
+                selected = false,
+                onClick = { /* Handle notifications navigation */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = "Favorite") },
+                selected = false,
+                onClick = { /* Handle notifications navigation */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Menu, contentDescription = "Menu") },
+                selected = false,
+                onClick = { /* Handle notifications navigation */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Settings, contentDescription = "Settings") },
+                selected = false,
+                onClick = { /* Handle notifications navigation */ }
+            )
+        }
     }
-
-}
-
-
-
-@Composable
-fun BottomNavigationBar() {
-    BottomNavigation {
-        BottomNavigationItem(
-            icon = {  },
-            label = { Text("Home3") },
-            selected = false,
-            onClick = { /* Handle home navigation */ }
-        )
-        BottomNavigationItem(
-            icon = { },
-            label = { Text("Home 4") },
-            selected = false,
-            onClick = { /* Handle search navigation */ }
-        )
-        BottomNavigationItem(
-            icon = {  },
-            label = { Text("Home 5") },
-            selected = false,
-            onClick = { /* Handle notifications navigation */ }
-        )
-    }
-
-
 }
