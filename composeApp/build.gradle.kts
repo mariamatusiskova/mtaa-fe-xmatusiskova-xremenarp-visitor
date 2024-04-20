52c84ae5bfe1a327a5ea219b241e5df07ba3edea
mariamatusiskova <xmatusiskova@stuba.sk>
2024-04-20 21:32:28 +0200

    delete unused dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -80,8 +80,5 @@ android {
 dependencies {
     implementation(libs.volley)
     implementation(libs.androidx.activity.ktx)
-
-    val nav_version = "2.7.6"
-    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
 
