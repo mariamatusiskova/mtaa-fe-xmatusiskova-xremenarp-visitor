58a075453e661d8af8422307e7805ea4b0282eae
PeterR29 <peto.remenar123@gmail.com>
2024-04-22 16:51:26 +0200

    tablet_mobil

--- composeApp/src/commonMain/kotlin/menu/Navigate.kt
+++ composeApp/src/commonMain/kotlin/menu/Navigate.kt
@@ -7,12 +7,12 @@ import androidx.compose.runtime.setValue
 
 class NavigationState {
     var signup by mutableStateOf(false)
-    var login by mutableStateOf(false)
+    var login by mutableStateOf(true)
     var resetPassword by mutableStateOf(false)
     var allPlaces by mutableStateOf(false)
     var favouritePlaces by mutableStateOf(false)
     var gpsPlaces by mutableStateOf(false)
-    var editProfile by mutableStateOf(true)
+    var editProfile by mutableStateOf(false)
     var settings by mutableStateOf(false)
     var category by mutableStateOf(false)
     var darkmode by mutableStateOf(false)
